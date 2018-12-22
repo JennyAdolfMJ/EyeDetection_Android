d411c4da0fbcdd3bac16d8e22fe317cc0009e3f1
Jenny <88904004@qq.com>
2018-12-22 15:36:54 +0800

    Eye detection

--- app/src/main/java/com/allpet/eyedetection_android/DetectionActivity.java
+++ app/src/main/java/com/allpet/eyedetection_android/DetectionActivity.java
@@ -1,5 +1,6 @@
 package com.allpet.eyedetection_android;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
@@ -10,6 +11,17 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -18,6 +30,11 @@ import org.opencv.core.Mat;
 public class DetectionActivity extends AppCompatActivity  implements CvCameraViewListener2 {
 
     private CameraBridgeViewBase mOpenCvCameraView;
+    private File mCascadeFile;
+    private CascadeClassifier mEyeCascade;
+    private Mat mRgba;
+    private Mat mGray;
+    private int absoluteFaceSize = 0;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -25,6 +42,33 @@ public class DetectionActivity extends AppCompatActivity  implements CvCameraVie
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS:
                 {
+                    try {
+                        // load cascade file from application resources
+                        InputStream is = getResources().openRawResource(R.raw.haarcascade_eye);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        mCascadeFile = new File(cascadeDir, "haarcascade_eye.xml");
+                        FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            os.write(buffer, 0, bytesRead);
+                        }
+                        is.close();
+                        os.close();
+
+                        mEyeCascade = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                        if (mEyeCascade.empty())
+                        {
+                            mEyeCascade = null;
+                        }
+
+                        cascadeDir.delete();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                     mOpenCvCameraView.enableView();
                 } break;
                 default:
@@ -68,6 +112,27 @@ public class DetectionActivity extends AppCompatActivity  implements CvCameraVie
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+        mRgba = inputFrame.rgba(); //RGBA
+        mGray = inputFrame.gray(); //单通道灰度图
+
+        if (absoluteFaceSize == 0) {
+            int height = mGray.rows();
+            if (Math.round(height * 0.2f) > 0) {
+                absoluteFaceSize = Math.round(height * 0.2f);
+            }
+        }
+
+        //检测并显示
+        MatOfRect faces = new MatOfRect();
+        if (mEyeCascade != null) {
+            mEyeCascade.detectMultiScale(mGray, faces, 1.1, 2, 2, new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+        }
+        Rect[] facesArray = faces.toArray();
+        if (facesArray.length > 0){
+            for (int i = 0; i < facesArray.length; i++) {    //用框标记
+                Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
+            }
+        }
+        return mGray;
     }
 }
